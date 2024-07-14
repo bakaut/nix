13e6421f330ecf9f9bf419bf0c4dc0934e5736cd
Nikolay Lebedev <fifo.mail@gmail.com>
2024-07-14 19:32:01 +0200

    add flux

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ curl -L https://nixos.org/nix/install | sh -s -- --daemon
 mkdir ~/.config/ || true
 cd ~/.config/
 git clone git@github.com:bakaut/nix.git .
+mkdir -p /nix/bin
+sudo chown -r _nixbld1:staff /nix/bin # nix user, main user group
 ```
 
 Tested on MacOs _version_
--- nix/flake.nix
+++ nix/flake.nix
@@ -1,15 +1,61 @@
 {
-  description = "A very basic flake";
+  description = "A template flake to install software from a URL with a specified SHA256";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv/latest";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, unstable, devenv }: {
+    packages = {
+      "aarch64-darwin" = let
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        unstablePkgs = import unstable { system = "aarch64-darwin"; };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        # Helper function to create a package from a URL and SHA256
+        mkPackage = { name, url, sha256 }: pkgs.stdenv.mkDerivation {
+          inherit name;
+          src = pkgs.fetchurl {
+            inherit url sha256;
+          };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+          buildInputs = [ pkgs.libarchive ];
 
+          phases = [ "unpackPhase" "installPhase" ];
+
+          unpackPhase = ''
+            runHook preUnpack
+            mkdir -p $TMPDIR/tmp
+            tar tzf $src
+            tar xzf $src -C $TMPDIR/tmp
+            runHook postUnpack
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            cp $TMPDIR/tmp/${name} $out/bin/
+            cp $out/bin/${name} /nix/bin/
+            chmod +x /nix/bin/${name}
+            runHook postInstall
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A custom package for ${name}.";
+            homepage = "https://example.com";
+            license = licenses.asl20;
+            platforms = platforms.all;
+          };
+        };
+
+      in {
+        flux = mkPackage {
+          name = "flux";
+          url = "https://github.com/fluxcd/flux2/releases/download/v2.2.3/flux_2.2.3_darwin_arm64.tar.gz";
+          sha256 = "JSn7XruBDOZmYmI1bik5goKnp2bfTn6VypOkD7gC9FI=";
+        };
+      };
+    };
   };
 }
